a11f93ebda0a3172cb6c6697be88c35b32d38f30
Matheus Samuel <60487041+zMatheus222@users.noreply.github.com>
2023-10-29 23:53:12 -0300

    Update compile.sh

--- data/compile.sh
+++ data/compile.sh
@@ -2,17 +2,17 @@
 
 # Nome do arquivo de código-fonte C++
 cpp_file="main.cpp"
-hpp_file="main.hpp"
+h_file="main.h"
 
 # Nome do arquivo objeto de saída
 cpp_output_file="main_cpp.o"
-hpp_output_file="main_hpp.o"
+h_output_file="main_h.o"
 
 # Compilação usando o GCC
 #g++ -o "$output_file" "$cpp_file"
 
 g++ -std=c++11 -o "$cpp_output_file" "$cpp_file"
-g++ -std=c++11 -o "$hpp_output_file" "$hpp_file"
+g++ -std=c++11 -o "$h_output_file" "$h_file"
 
 # Verificar se a compilação foi bem-sucedida
 if [ $? -eq 0 ]; then
